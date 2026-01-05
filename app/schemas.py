71682cd3aa5013a78189bb4a69b395566f941ebf
Yulia Andreeva <yn.andreeva@mail.ru>
2026-01-05 15:39:01 +0300

    schemas.py fix

--- app/schemas.py
+++ app/schemas.py
@@ -7,3 +7,9 @@ class UserRegister(BaseModel):
 class AdCreate(BaseModel):
     title: str
     description: str
+
+    @field_validator("title")
+    def validate_title(cls, v):
+        """Проверка длины заголовка"""
+        if len(v) < 3 or len(v) > 100:
+            raise ValueError("Длина заголовка должна быть от 3 до 100 символов")
